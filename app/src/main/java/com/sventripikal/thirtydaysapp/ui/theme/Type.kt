085196a79ad6b4d8d1ea02157b25e818aa12785f
Jose Garza <sventripikal@gmail.com>
2024-09-16 14:57:12 -0500

    Quick Updates
    -added Poppins Italic font
    -updated Image & string Reference

--- app/src/main/java/com/sventripikal/thirtydaysapp/ui/theme/Type.kt
+++ app/src/main/java/com/sventripikal/thirtydaysapp/ui/theme/Type.kt
@@ -4,14 +4,16 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.sventripikal.thirtydaysapp.R
 
 // Poppins font family
 val Poppins = FontFamily(
+    Font(resId = R.font.poppins_bold, weight = FontWeight.Bold),
     Font(resId = R.font.poppins_regular, weight = FontWeight.Normal),
-    Font(resId = R.font.poppins_bold, weight = FontWeight.Bold)
+    Font(resId = R.font.poppins_italic, style = FontStyle.Italic)
 )
 
 // different font styles using Poppins font
@@ -28,7 +30,7 @@ val Typography = Typography(
     ),
     labelSmall = TextStyle(
         fontFamily = Poppins,
-        fontWeight = FontWeight.Normal,
+        fontStyle = FontStyle.Italic,
         fontSize = 11.sp
     )
 )
